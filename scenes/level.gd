288d18d8cb8a9f626b4ffd562d4514eba49bb269
Vladimir Golubev <mrgolubeff@icloud.com>
2024-05-15 11:31:30 +0300

    Add katakana array & show syllables in frames
    Also change Button to Area2D in the Syllable scene.

--- scenes/level.gd
+++ scenes/level.gd
@@ -4,6 +4,55 @@ extends Node
 @export var syllable_scene: PackedScene
 var upper_syllables: Array = []
 
+var katakana = [
+	["ア", "a"], # 0
+	["イ", "i"],
+	["ウ", "u"],
+	["エ", "e"],
+	["オ", "o"],
+	["カ", "ka"], # 5
+	["キ", "ki"],
+	["ク", "ku"],
+	["ケ", "ke"],
+	["コ", "ko"],
+	["サ", "sa"], # 10
+	["シ", "shi"],
+	["ス", "su"],
+	["セ", "se"],
+	["ソ", "so"],
+	["タ", "ta"], # 15
+	["チ", "chi"],
+	["ツ", "tsu"],
+	["テ", "te"],
+	["ト", "to"],
+	["ナ", "na"], # 20
+	["ニ", "ni"],
+	["ヌ", "nu"],
+	["ネ", "ne"],
+	["ノ", "no"],
+	["ハ", "ha"], # 25
+	["ヒ", "hi"],
+	["フ", "fu"],
+	["ヘ", "he"],
+	["ホ", "ho"],
+	["マ", "ma"], # 30
+	["ミ", "mi"],
+	["ム", "mu"],
+	["メ", "me"],
+	["モ", "mo"],
+	["ヤ", "ya"], # 35
+	["ユ", "yu"],
+	["ヨ", "yo"],
+	["ラ", "ra"], # 38
+	["リ", "ri"],
+	["ル", "ru"],
+	["レ", "re"],
+	["ロ", "ro"],
+	["ワ", "wa"], # 43
+	["ヲ", "wo"], # 44
+	["ン", "n"] # 45
+]
+
 
 func _ready():
 	create_syllable()
@@ -23,16 +72,16 @@ func create_syllable():
 	var syllable = syllable_scene.instantiate()
 	syllable.position.x = -100
 	syllable.position.y = 50
-	syllable.pressed.connect(_on_syllable_pressed.bind(syllable))
+	syllable.get_node("Button").pressed.connect(_on_syllable_pressed.bind(syllable))
 	add_child(syllable)
 	upper_syllables.append(syllable)
 
 
-func _on_syllable_pressed(button: Button):
+func _on_syllable_pressed(button: Area2D):
 	print("Syllable pressed.")
 	print(button.position.x)
 	button.position.y += 300
-	button.disabled = true
+	button.get_node("Button").disabled = true
 	var button_index = upper_syllables.find(button)
 	var roll = randf()
 	if roll >= 0.6:
